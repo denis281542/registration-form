01383487c1cd9325c91292fb7d6ba7cfaf9c73ae
Denis Kuimov <deniskuimov@mail.ru>
2020-08-02 15:21:57 +0500

    Added validation sing in form

---
+++ assets/js/validation.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("sing-in-form");
+
+const submitForm = (event) => {
+    event.preventDefault();
+    const name = event.target.querySelector("input[type=text]").value;
+    const nameHint = event.target.querySelector(".input-field");
+    const password = event.target.querySelector("input[type=password]").value;
+    const passwordHint = event.target.querySelector(".input-field-password");
+
+    const isValidName = validateName(name);
+    if (!isValidName) {
+        nameHint.classList.add("input-field--active");
+        return;
+    }
+    if (nameHint.classList.contains("input-field--active")) {
+        nameHint.classList.remove("input-field--active");
+    }
+
+    const isValidPassword = validatePassword(password);
+    if (!isValidPassword) {
+        passwordHint.classList.add("input-field-password--active");
+        return;
+    }
+    if (passwordHint.classList.contains("input-field-password--active")) {
+        passwordHint.classList.remove("input-field-password--active");
+    }
+
+};
+
+form.addEventListener("submit", submitForm);
+
+function validateName(name) {
+    const re = /^([a-zA-Z ]){2,30}$/;
+    return re.test(name);
+}
+
+function validatePassword(password) {
+    const re = /^([a-zA-Z ]){2,30}$/;
+    return re.test(password);
+}
+
