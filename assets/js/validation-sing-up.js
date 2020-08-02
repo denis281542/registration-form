48c496b9ea6eb0b0a5dd09643b4b21abcadbc7ec
Denis Kuimov <deniskuimov@mail.ru>
2020-08-02 23:12:42 +0500

    Added validation email sing up form

--- assets/js/validation-sing-up.js
+++ assets/js/validation-sing-up.js
@@ -6,6 +6,8 @@ const submitFormSungUp = (event) => {
     const nameError = event.target.querySelector('.input-field');
     const email = event.target.querySelector('input[type="email"]').value;
     const emailError = event.target.querySelector('.input-field-email');
+    const password = event.target.querySelector('input[type="password"]').value;
+    const passwordError = event.target.querySelector('.input-field-password');
 
     const isValidName = validateName(name);
     if(!isValidName) {
@@ -24,6 +26,15 @@ const submitFormSungUp = (event) => {
     if(emailError.classList.contains('input-field-email--active')) {
         emailError.classList.remove('input-field-email--active');;
     };
+
+    const isValidpassword = validatePassword(password);
+    if(!isValidpassword) {
+        passwordError.classList.add('input-field-password--active');
+        return;
+    };
+    if(passwordError.classList.contains('input-field-password--active')) {
+        passwordError.classList.remove('input-field-password--active');;
+    };
 };
 
 formSingUp.addEventListener("submit", submitFormSungUp);
@@ -37,4 +48,9 @@ function validateName(name) {
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
+}
+
+function validatePassword(password) {
+    const re = /^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20})$/;
+    return re.test(password);
 }
